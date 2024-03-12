4fe5ee24b0327cab6ae83b444ff499013aa0d562
Olaoye Ademola <demmydd44@gmail.com>
2024-03-12 08:41:56 -0700

    print whether a random number is negative, zero or positive

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if (number < 0):
-    lastdig = (number % 10) - 10
+    ldig = (number % 10) - 10
 else:
-    lastdig = number % 10
-if (lastdig > 5):
-    print("Last digit of {} is {} and is greater than 5" .format(number, lastdig))
-elif (lastdig == 0):
-    print("Last digit of {} is {} and is 0" .format(number, lastdig))
-elif (lastdig < 6 and lastdig != 0):
-    print("Last digit of {} is {} and is less than 6 and not 0" .format(number, lastdig))
-
+    ldig = number % 10
+if (ldig > 5):
+    print("Last digit of {} is {} and is greater than 5" .format(number, ldig))
+elif (ldig == 0):
+    print("Last digit of {} is {} and is 0" .format(number, ldig))
+elif (ldig < 6 and ldig != 0):
+    print(f"Last digit of {number} is {ldig} and is less than 6 and not 0")
