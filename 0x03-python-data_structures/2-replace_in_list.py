6825a9079869dacc25f34babb767a487ba37526b
Olaoye Ademola <demmydd44@gmail.com>
2024-03-16 02:11:49 -0700

    replace an element of a list at a specific position

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         exit(None)
     my_list[idx] = element
     return(my_list)
-    """print("Element at index {} is {}".format(idx, my_list[idx - 1]))"""
