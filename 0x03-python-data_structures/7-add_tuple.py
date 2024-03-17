524a2211414e2352db4944bee4536f63fad26a79
Olaoye Ademola <demmydd44@gmail.com>
2024-03-17 01:03:24 -0700

    testin tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    for num in range(2):
-        if len(tuple_a) >= 1:
-            a = tuple_a[0]
-        else:
-            a = 0
-        if len(tuple_b) >= 1:
-            a += tuple_b[0]
-        else:
-            a += 0
-        if len(tuple_a) >= 2:
-            b = tuple_a[1]
-        else:
-            b = 0
-        if len(tuple_b) >= 2:
-            b += tuple_a[1]
-        else:
-            b += 0
-        new_tuple = a, b
-        return new_tuple
+    for i in range(2):
+        a = tuple_a[0] if len(tuple_a) >= 1 else 0
+        a += tuple_b[0] if len(tuple_b) >= 1 else 0
+
+        b = tuple_a[1] if len(tuple_a) >= 2 else 0
+        b += tuple_b[1] if len(tuple_b) >= 2 else 0
+
+        t = a, b
+        return t
