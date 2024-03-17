0729020cc5b78404e65397cdf839131cc980b8b1
Olaoye Ademola <demmydd44@gmail.com>
2024-03-17 11:45:47 -0700

    print a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,13 @@ def print_matrix_integer(matrix=[[]]):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), 
                     end='' if j - len(matrix[i]) == -1 else ' ')
-        print()
+        print("$")
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
