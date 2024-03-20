7d0bb16a686dcc77ffb56d34d02e093492e67fff
Olaoye Ademola <demmydd44@gmail.com>
2024-03-20 13:52:24 -0700

    return a new dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
-    list_keys = list(new_dir.keys())
+    list_keys = list(new_dict.keys())
 
     for num in list_keys:
         new_dict[num] *= 2
