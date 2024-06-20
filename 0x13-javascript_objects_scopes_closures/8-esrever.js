a8a83c5c14f0f3050973d72b9c93687d95cc3907
Olaoye Ademola <demmydd44@gmail.com>
2024-06-20 16:12:13 -0700

    return the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
 exports.esrever = function (list) {
   const reverse_list = [];
 
   for (let i = list.length - 1; i >= 0; --i) {
     reverse_list.push(list[i]);
   }
-
   return reverse_list;
 };
